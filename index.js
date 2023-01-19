ffe32be692caae476e351293da3c40d63f7e53e4
mineingvps <bosscraft500@gmail.com>
2023-01-19 16:01:11 +0700

    startproject

---
+++ index.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
