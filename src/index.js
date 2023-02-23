8461bd0a669cba6026a0723b9eccb555fb34dd09
mineingvps <bosscraft500@gmail.com>
2023-02-23 13:44:09 +0700

    dd

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World! I am Happy</h1>");
+  res.send("<h1>Hello World!</h1>");
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
